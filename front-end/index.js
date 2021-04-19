1f5bc2474107d456bb1d574212b4c8fc75da6870
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-04-19 14:56:29 -0300

    Front-end: Moved Javascript code to a dedicated folder

--- front-end/index.js
+++
@@ -1,2 +0,0 @@
-// TODO: This is needed so the "dom.js"'s body is executed.
-import { searchInput, searchIcon } from "/source-code/dom.js";
---
+++ front-end/source-code/javascript/index.js
@@ -0,0 +1,2 @@
+// TODO: This is needed so the "dom.js"'s body is executed.
+import { searchInput, searchIcon } from "./dom.js";
