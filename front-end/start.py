4dcc9333dfccf142796dbb070c450047ad876131
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-04-27 01:38:06 -0300

    Front-end: Added a small script to start the front-end

---
+++ front-end/start.py
@@ -0,0 +1,28 @@
+import asyncio
+
+# Runs a specific command in a separate subprocess.
+async def run(command):
+    process = await asyncio.create_subprocess_shell(
+            command,
+            stdout = asyncio.subprocess.PIPE,
+            stderr = asyncio.subprocess.PIPE
+    )
+
+    output, errors = await process.communicate()
+
+    print(f'[{command!r} exited with {process.returncode}]')
+
+    if output:
+        print(f'[output]\n{output.decode()}')
+
+    if errors:
+        print(f'[errors]\n{errors.decode()}')
+
+# Runs the front-end related commands.
+async def run_front_end_commands():
+    await asyncio.gather(
+            run("cd source-code && live-server --entry-file=html/index.html"),
+            run("cd source-code && sass --watch sass:css")
+    )
+
+asyncio.run(run_front_end_commands())
