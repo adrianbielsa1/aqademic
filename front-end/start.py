70a20937992144ca2a4ce719e572a51711e60016
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-05-04 19:49:21 -0300

    Front-end: Programmer-oriented improvements and URL aliasing in "start.py"
    Now it is a little bit prettier to add new arguments and commands to execute.
    The script now prints the commands it ran (and their arguments).
    Also, HTML files may use the new `mount`ed URL aliases to avoid displaying the site's folder structure in the URL.

--- front-end/start.py
+++ front-end/start.py
@@ -18,11 +18,57 @@ async def run(command):
     if errors:
         print(f"[errors]\n{errors.decode()}")
 
+# Auxiliar function to convert a dictionary into a flattened string, adding
+# a = symbol between each key and its value, and a space between each key-value
+# pair.
+#
+# Example input & output:
+#
+#   [in]    { a: 1, b: 2, c: [3, 4] }
+#   [out]   a=1 b=2 c=3 c=4
+def flatten_dictionary_into_string(d, concatenator, separator):
+    s = ""
+
+    for key, value in d.items():
+        if isinstance(value, str):
+            s += key + concatenator + value
+        elif isinstance(value, list):
+            for inner_value in value:
+                s += key + concatenator + inner_value
+
+        # A space to separate each argument.
+        s += separator
+
+    return s
+
 # Runs the front-end related commands.
 async def run_front_end_commands():
-    await asyncio.gather(
-            run("live-server --entry-file=source-code/html/index.html"),
-            run("cd source-code && sass --watch sass:css")
-    )
+    live_server_arguments = {
+        "--port": "8080",
+        "--entry-file": "source-code/html/index.html",
+
+        # A few aliases so the URL doesn't show the inner folder structure
+        # unless explicitly accessed.
+        "--mount": [
+            "/login:source-code/html/login.html",
+        ],
+    }
+
+    sass_arguments = {
+        "--watch": "source-code/sass:source-code/css"
+    }
+
+    commands = [
+        "live-server " + flatten_dictionary_into_string(live_server_arguments, "=", " "),
+        "sass " + flatten_dictionary_into_string(sass_arguments, " ", " ")
+    ]
+
+    # Display the list of commands.
+    for command in commands:
+        print("[run] " + command)
+
+    # Apply the `run` function to each `command`, and then unpack these async
+    # calls and wait for them to complete with `gather`.
+    await asyncio.gather(*map(run, commands))
 
 asyncio.run(run_front_end_commands())
