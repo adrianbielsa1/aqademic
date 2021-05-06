fe925d29f1342df7439a99953b4cbe6d972775da
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-05-06 15:56:26 -0300

    Front-end: Replaced the old "dom.js" module with an improved version
    The new version features better error tolerance/notification mechanisms in case the DOM isn't loaded yet, events to interact with other modules, and better wrappers for DOM elements.

---
+++ front-end/source-code/javascript/helpers/dom.js
@@ -0,0 +1,66 @@
+// This module handles as much direct interaction with the DOM as possible,
+// so other modules can interact with it through custom events or functions.
+
+class DOMNotLoadedError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "DOMNotLoadedError";
+    }
+}
+
+export const getSearchInputElement = () => {
+    if (document.readyState === "loading") {
+        throw new DOMNotLoadedError("");
+    }
+
+    return document.querySelector(".page > .search > .query > .bar > .input");
+};
+
+export const getSearchButtonElement = () => {
+    if (document.readyState === "loading") {
+        throw new DOMNotLoadedError("");
+    }
+
+    return document.querySelector(".page > .search > .query > .bar > .button");
+};
+
+// Gathers the current input query and dispatches an event containing
+// said information so other objects can potentially transfer the request
+// to the server.
+const dispatchStartRequestEvent = function() {
+    const input = getSearchInputElement();
+
+    document.dispatchEvent(new CustomEvent("customStartRequest", { detail: input.value }));
+}
+
+const mount = function() {
+    const input = getSearchInputElement();
+    const button = getSearchButtonElement();
+
+    input.addEventListener("keyup", (e) => {
+        // TODO: It would be good to only dispatch an event when the length changes.
+        // Currently, if you press ANY key (such as shift) and the input's length
+        // exceeds the threshold, the event will be dispatched no matter what.
+        //
+        // NOTE: 13 is the code for the "ENTER" key.
+        //
+        // This checks for the length of the string written in the input field.
+        if (input.value.length > 4) {
+            dispatchStartRequestEvent();
+        } else if (e.keyCode === 13) {
+            dispatchStartRequestEvent();
+        }
+    });
+
+    button.addEventListener("click", (e) => {
+        dispatchStartRequestEvent();
+    });
+};
+
+if (document.readyState !== "loading") {
+    mount();
+} else {
+    document.addEventListener("DOMContentLoaded", () => {
+        mount();
+    });
+}
