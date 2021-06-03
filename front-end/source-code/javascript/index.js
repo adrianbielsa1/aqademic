64051ed6d043faf7b2902797fa8c546b6fc1932a
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-06-03 09:30:52 -0300

    Front-end: Now results of a request are rendered on arrival using Preact

--- front-end/source-code/javascript/index.js
+++ front-end/source-code/javascript/index.js
@@ -1,2 +1,18 @@
-// TODO: This is needed so the "dom.js"'s body is executed.
-import { searchInput, searchIcon } from "./dom.js";
+import {} from "./helpers/request.js"; // This is needed so "request.js"'s body is executed.
+import { getSearchResultListElement } from "./helpers/dom.js";
+import { buildless } from "./helpers/buildless.js";
+import { ResultList } from "./components/result-list.js";
+
+// Called whenever a request is resolved.
+document.addEventListener("customResolvedRequest", (event) => {
+    // Extract the request's response.
+    const response = event.detail;
+
+    // Where we'll render the `ResultList` component.
+    const domResultList = getSearchResultListElement();
+
+    // Construct the `ResultList` component and render it.
+    buildless.render(buildless.html`
+        <${ ResultList } results=${ response }/>
+    `, domResultList);
+});
