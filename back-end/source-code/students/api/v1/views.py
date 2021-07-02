c437756f805f60bd6d669e259a55f7784e0a20e3
Adrián Bielsa <63594921+adrianbielsa1@users.noreply.github.com>
2021-07-02 15:44:26 -0300

    Back-end: Replaced class-based API views with function-based ones

--- back-end/source-code/students/api/v1/views.py
+++ back-end/source-code/students/api/v1/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,42 +6,35 @@ from rest_framework import permissions
 from ...models import StudentModel
 from ...serializers import StudentSerializer
 
+"""
+Returns a list of students matching the specified names.
+"""
+@api_view(["GET"])
+def read_by_names(request, expected_names):
+    # Get all students from the database.
+    students = StudentModel.objects.all()
+    
+    # Filter the students whose name does not contain the current expected
+    # name. Then, do it again - shrinking the list of students at each
+    # iteration. In the end we get only those students whose name contains
+    # ALL of the expected names.
+    for current_expected_name in expected_names.split("-"):
+        students = students.filter(names__icontains=current_expected_name)
 
-class StudentsByNamesAPIView(APIView):
-    def get(self, request, expectedNames, *args, **kwargs):
-        """
-        Returns a list of students matching the specified names.
-        """
-        expectedNames = expectedNames.split("-")
+    # Serialize the results and send them back.
+    serializer = StudentSerializer(students, many = True)
+    return Response(serializer.data, status = status.HTTP_200_OK)
 
-        # First get all of the students from the database.
-        students = StudentModel.objects.all()
-
-        # Now get students that have the names we've been requested.
-        for currentExpectedName in expectedNames:
-            students = students.filter(names__icontains=currentExpectedName)
-
-        # Serialize the results.
-        serializer = StudentSerializer(students, many = True)
-
-        return Response(serializer.data, status = status.HTTP_200_OK)
-
-class StudentsByFileAPIView(APIView):
-    def get(self, request, expectedFile, *args, **kwargs):
-        """
-        Returns a list of students matching the specified file. There should
-        be at most one student with said file.
-        """
-        try:
-            # Find the student in the database.
-            student = StudentModel.objects.get(file = expectedFile)
-        except StudentModel.DoesNotExist:
-            return Response(
-                { "error": "No student found with file: " + str(expectedFile) },
-                status = status.HTTP_400_BAD_REQUEST
-            )
-        else:
-            # Serialize the student.
-            serializer = StudentSerializer(student)
-
-            return Response(serializer.data, status = status.HTTP_200_OK)
+"""
+Returns a list of students matching the specified file. Unless there's
+something wrong with the database (two students with the same file), the
+list should contain a single value.
+"""
+@api_view(["GET"])
+def read_by_file(request, expected_file):
+    # Find the students whose file matches.
+    students = StudentModel.objects.all().filter(file = expected_file)
+    
+    # Serialize the results and send them back.
+    serializer = StudentSerializer(students, many = True)
+    return Response(serializer.data, status = status.HTTP_200_OK)
